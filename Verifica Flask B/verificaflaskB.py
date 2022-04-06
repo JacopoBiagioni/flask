f58d033437db8049a665189228c676a5277813a3
JacopoBiagioni <biagioni.jacopo@iisgalvanimi.edu.it>
2022-04-06 12:18:52 +0000

    finito

--- Verifica Flask B/verificaflaskB.py
+++ Verifica Flask B/verificaflaskB.py
@@ -35,9 +35,48 @@ def scelta():
 @app.route('/stazioniradio', methods=['GET'])
 def stazioniradio():
     quartieriRadio = request.args['quartiere']
-    quartiereUtente = quartieri[quartieri['NIL'] == quartieriRadio]
+    quartiere_utente = quartieri[quartieri['NIL'] == quartieriRadio]
+    stazioni_quartieri = stazionigeo[stazionigeo.within(quartiere_utente.geometry.squeeze())]
+    return render_template('elenco.html', risultato = stazioni_quartieri.to_html())
+
+@app.route('/nome', methods=['GET'])
+def nome():
+    return render_template('nome.html')
+
+@app.route('/risultato', methods=['GET'])
+def risultato():
+    global quartiereUtente, stazioniQuartieri
+    quartiere = request.args['quartiere']
+    quartiereUtente = quartieri[quartieri['NIL'].str.contains(quartiere)]
     stazioniQuartieri = stazionigeo[stazionigeo.within(quartiereUtente.geometry.squeeze())]
-    return render_template('elenco.html', risultato = stazioniQuartieri.to_html())
+    return render_template('mappa.html')
+
+@app.route('/mappa', methods=['GET'])
+def mappa():
+    fig, ax = plt.subplots(figsize = (12,8))
+    quartiereUtente.to_crs(epsg=3857).plot(ax=ax, alpha=0.5, edgecolor='k')
+    stazioniQuartieri.to_crs(epsg=3857).plot(ax=ax, color='k')
+    ctx.add_basemap(ax=ax)
+    output = io.BytesIO()
+    FigureCanvas(fig).print_png(output)
+    return Response(output.getvalue(), mimetype='image/png')
+
+@app.route('/numero', methods=['GET'])
+def numero():
+  global risultato
+  risultato = stazioni.groupby('MUNICIPIO')['OPERATORE'].count().reset_index().sort_values(by='MUNICIPIO',ascending=True)
+  return render_template('numero.html', risultato = risultato.to_html())
+
+@app.route('/grafico', methods=['GET'])
+def grafico():
+  fig, ax = plt.subplots(figsize=(12,8))
+  x = risultato.MUNICIPIO
+  y = risultato.OPERATORE
+  ax.bar(x, y, color = "#304C89")
+  output = io.BytesIO()
+  FigureCanvas(fig).print_png(output)
+  return Response(output.getvalue(), mimetype='image/png')
+
 
 
 
